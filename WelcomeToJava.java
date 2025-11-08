528688999b8fdad0c978fbfb907355bb5503c841
Ahmadullah Rahimi <rahimirokhan@gmail.com>
2025-11-08 13:56:21 +0430

    Add files via upload

---
+++ WelcomeToJava.java
@@ -0,0 +1,15 @@
+
+package welcometojava;
+
+
+public class WelcomeToJava {
+
+   
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO JAVA");
+        System.out.println("welcome to programing");
+        System.out.println("programing is fun");
+     
+    }
+    
+}
