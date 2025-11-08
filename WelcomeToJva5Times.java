f02efc91248977e6940edbd6332e7df30347cbfe
Ahmadullah Rahimi <rahimirokhan@gmail.com>
2025-11-08 14:01:39 +0430

    Add files via upload

---
+++ WelcomeToJva5Times.java
@@ -0,0 +1,14 @@
+
+package welcometojva5times;
+
+
+public class WelcomeToJva5Times {
+
+ 
+    public static void main(String[] args) {
+     for(int count = 1 ; count<=5; count++){
+         System.out.println("Welcome to java");
+     }
+    }
+    
+}
